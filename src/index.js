98867b596962c57d82d8a0a368058707ef84bf96
jeff <jmorton138@gmail.com>
2022-01-09 15:49:36 +0000

    Added Menu tab functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import NavBar from './nav.js';
-
 import HomeDisplay from './home.js';
+import Menu from './menu.js';
 
 function pageLoad() {
     HomeDisplay();
     NavBar();
 
 }
+document.addEventListener('click', event => {
+    console.log(event.target.textContent);
+    if (event.target.textContent === 'Menu') {
+        Menu();
+    }
+})
 window.addEventListener('load', () => {
  pageLoad();
 })
---
+++ src/menu.js
@@ -0,0 +1,12 @@
+export default function Menu() {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+    const menu = document.createElement('div');
+    for (let i = 0; i < 10; i++) {
+        const item = document.createElement('div');
+        item.textContent = `Dish number ${i+1}`;
+        menu.appendChild(item);
+    }
+    content.appendChild(menu);
+
+}
