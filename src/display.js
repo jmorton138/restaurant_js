b0e2a1727845a2675632667774e4be3090419834
jeff <jmorton138@gmail.com>
2022-01-09 11:23:38 +0000

    Config and add image to hero

--- src/display.js
+++ src/display.js
@@ -1,14 +1,27 @@
+import Picture from './picture.jpg';
+import './style.css';
+
 export default function display() {
     console.log('toodles');
 
     const content = document.getElementById('content');
     const hero = document.createElement('div')
     hero.className = 'hero';
+
     const title = document.createElement('h1');
     title.textContent = "Food Place";
-    hero.appendChild(title)
-    const body = document.createElement('div')
+
+    hero.appendChild(title);
+    
+    const pic = new Image();
+    pic.className = "image";
+    pic.src = Picture;
+    hero.appendChild(pic);
+
+    const body = document.createElement('div');
     body.textContent = "This is the best place for food.";
+
     content.appendChild(hero);
+    content.appendChild(pic)
     content.appendChild(body);
 }
